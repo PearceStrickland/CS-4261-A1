4e4cc9da5eef80a743cd7d49ce31fa80c7e829fc
PearceStrickland <78804764+PearceStrickland@users.noreply.github.com>
2024-01-29 13:16:29 -0500

    Set up so if email already exists in database then user cant create new account with same email

--- app.js
+++ app.js
@@ -4,13 +4,18 @@ require('./models/db');
 
 const User = require('./models/user')
 
+const email = 'check@gmail.com'
 
 const app = express()
 
 app.post('/create-user', async (req, res) => {
-    const isNewUser = await User.isThisEmailInUse('checj@gmail.com')
-    if(!isNewUser) return res.json
-     const user = await User({fullname: 'John', email: 'checj@gmail.com', password: '1234'})
+    const isNewUser = await User.isThisEmailInUse(email)
+    if(!isNewUser) 
+        return res.json({
+            success: false,
+            message: 'This email is already in use, try sign-in',
+        });
+     const user = await User({fullname: 'John', email: email, password: '1234'})
      await user.save();
      res.json(user)
 });
